5f48624240c16576877d5c1c9d5bb61e2a89c353
Abdias Cuellar <abdiascprado@gmail.com>
2025-05-24 17:12:41 -0400

    cerrar session admin dashboard

---
+++ dashboard_admin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/residente.css">
+</head>
+<body>
+    <a style="text-decoration:none; color:#fff;" href="logout.php">
+            <p class="btn-enlace">Cerrar sesión</p>
+        </a>
+</body>
+</html>
